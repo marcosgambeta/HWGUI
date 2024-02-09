5b881dfe64fb77a725d5b79c8cf93abd3727dce9
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2024-02-09 11:05:51 -0300

    Update c_ipaddr.c remove '#pragma warning(disable : 4312)'

--- source/c_ipaddr.c
+++ source/c_ipaddr.c
@@ -42,11 +42,6 @@
     Copyright 1999-2003, http://www.harbour-project.org/
 ---------------------------------------------------------------------------*/
 
-// TODO: revision
-#if defined(_MSC_VER)
-#pragma warning(disable : 4312)
-#endif
-
 #include "hwingui.h"
 #include "incomp_pointer.h"
 
