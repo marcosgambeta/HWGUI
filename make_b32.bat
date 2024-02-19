f8023087b552d46a269ff465a453caaafd5a8638
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2024-02-19 07:47:25 -0300

    Update make_b32.bat fix build

--- make_b32.bat
+++ make_b32.bat
@@ -9,7 +9,7 @@ if not exist obj\b32\mt md obj\b32\mt
 :BUILD
 
 rem set __XHARBOUR__=ON
-set ACTIVEX_SUPPORT=ON
+rem set ACTIVEX_SUPPORT=ON
 set CONTRIB_SUPPORT=ON
 make -l EXE_OBJ_DIR=obj\b32\bin OBJ_DIR=obj\b32 -fmakefile.bc %1 %2 %3 > make_b32.log
 if errorlevel 1 goto BUILD_ERR
